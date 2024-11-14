7f784206fa5e28566977c15e587f2013fbe48d75
Tribhuvan nath <tribhuvannath4567@gmail.com>
2024-11-15 00:25:21 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-3xl flex text-red-600 justify-center items-center min-h-screen">
+      Launching soon...
+    </div>
+  );
+};
+
+export default App;
